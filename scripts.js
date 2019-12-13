c6798f707db6d285a6467daa077956ed5798ccbb
Jack Nierras Beltran <jacknierras@Jacks-MacBook-Pro.local>
2019-12-13 01:18:29 +0100

    shop

--- scripts.js
+++ scripts.js
@@ -5,3 +5,17 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").fadeIn();
 
 });
+
+
+$(function () {
+    var selectedClass = "";
+    $(".filter").click(function () {
+        selectedClass = $(this).attr("data-rel");
+        $("#gallery").fadeTo(100, 0.1);
+        $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
+        setTimeout(function () {
+            $("." + selectedClass).fadeIn().addClass('animation');
+            $("#gallery").fadeTo(300, 1);
+        }, 300);
+    });
+});
