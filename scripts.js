46183c3f0b838e11177fa279a202140225f8bf8b
Jack Nierras Beltran <jacknierras@Jacks-MacBook-Pro.local>
2019-12-16 13:51:51 +0100

    næsten der

--- scripts.js
+++ scripts.js
@@ -5,17 +5,3 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").fadeIn();
 
 });
-
-
-$(function () {
-    var selectedClass = "";
-    $(".filter").click(function () {
-        selectedClass = $(this).attr("data-rel");
-        $("#gallery").fadeTo(100, 0.1);
-        $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
-        setTimeout(function () {
-            $("." + selectedClass).fadeIn().addClass('animation');
-            $("#gallery").fadeTo(300, 1);
-        }, 300);
-    });
-});
